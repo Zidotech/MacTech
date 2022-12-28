6383dc4646de4e2c552a312e05528b7ba298b2c3
Zulu <adekunmbi18@gmail.com>
2022-12-28 04:23:39 -0800

    MacTech Website Demo

---
+++ JS/form.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  centerSlide: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
